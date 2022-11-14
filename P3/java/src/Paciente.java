45b6a169c369ebc435b69054c915fc52e7924acd
sergioM <sermorepe@gmail.com>
2022-11-14 12:15:09 +0100

    a

--- P3/java/src/Paciente.java
+++ P3/java/src/Paciente.java
@@ -1,2 +1,22 @@
+import java.util.List;
+
 public class Paciente {
+
+    Paciente(){
+        List<Object> listaExpedientes = new List<Object>();
+    }
+
+    public List<Expediente> getExpedientes(){
+        //Devuelve todos los expedientes que ha tenido un paciente
+
+        return null;
+    }
+
+    public Expediente getExpedienteAbierto(){
+        //Si tiene un expediente abierto, lo devuelve
+
+        return null;
+    }
+
+
 }
