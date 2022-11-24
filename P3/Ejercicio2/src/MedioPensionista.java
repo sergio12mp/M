d7db9878c0d119c36e66ebd3a6838f54c7c85a8b
nachogarcia <iggguvs@uma.es>
2022-11-24 13:54:55 +0100

    ejercicio 2 planteado

---
+++ P3/Ejercicio2/src/MedioPensionista.java
@@ -0,0 +1,12 @@
+public class MedioPensionista {
+    private Pensionista pensionista;
+    private Activo activo;
+    public MedioPensionista(Pensionista pensionista, Activo activo){
+        this.pensionista = pensionista;
+        this.activo = activo;
+    }
+
+    public void incrementar(){
+
+    }
+}
--- P3/Ejercicio2/src/Trabajador.java
+++ P3/Ejercicio2/src/Trabajador.java
@@ -1,3 +1,19 @@
 public class Trabajador {
+
+    public String nombre;
+    public String numeroSeguridadSocial;
+    protected double salario;
+    public Trabajador(String nombre, String numeroSeguridadSocial, double Salario){
+        this.nombre = nombre;
+        this.numeroSeguridadSocial = numeroSeguridadSocial;
+        this.salario = salario;
+    }
+    public double nomina(){
+        double nomina = salario;
+        return nomina;
+    } 
+    public void incrementar(){
+
+    }
     
 }
