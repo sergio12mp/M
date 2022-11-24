8e2c45776807a569224c5cb7ed0fddbfe7214e91
Carlos-DE <carlosdominguezenriquez611@gmail.com>
2022-11-24 12:58:05 +0100

    acceso

--- P3/java/src/Acceso.java
+++ P3/java/src/Acceso.java
@@ -3,13 +3,19 @@ import java.util.Date;
 public class Acceso {
     private Date fecha;
     TipoAcceso tipo;
+    private Profesional pro;
+    private Expediente exp;
 
-    public Acceso(Date fecha,TipoAcceso  tipo){
+    public Acceso(Date fecha,TipoAcceso  tipo, Profesional pro, Expediente exp){
         assert(fecha!=null);
         assert(tipo!=null);
+        assert(exp!=null);
+        assert(pro!=null);
 
-        this.fecha = fecha;
-        this.tipo = tipo;
+        setDate(fecha);
+        setTipoAcceso(tipo);
+        setProfesional(pro);
+        setExpediente(exp);
     }
 
     //GETTERS
@@ -21,12 +27,33 @@ public class Acceso {
         return tipo;
     }
 
+    public Expediente getExpediente() {
+        return exp;
+    }
+
+    public Profesional getProfesional(){
+        return pro;
+    }
+    
     //SETTERS
     public void setDate(Date nuevaFecha){
+        assert(nuevaFecha!=null);
         fecha = nuevaFecha;
     }
 
     public void setTipoAcceso(TipoAcceso nuevoTipo){
+        assert(nuevoTipo!=null);
         tipo = nuevoTipo;
     }
+
+    public void setProfesional(Profesional pro) {
+        assert pro!=null;
+        this.pro = pro;
+        }
+    
+    public void setExpediente(Expediente exp) {
+        assert exp!=null;
+        this.exp = exp;
+     }
+            
 }
