d174f8d02e6c9ef76a304d00d507bccb2a632865
SprenBonded <antonio22ortega@gmail.com>
2022-11-24 13:02:07 +0100

    expediente

--- P3/java/src/Expediente.java
+++ P3/java/src/Expediente.java
@@ -1,10 +1,40 @@
 import java.util.*;
 
 public class Expediente {
-    private Paciente paciente;
-    private List<Acceso> acceso;
+    private Diagnostico diagnostico;
+    private List<Acceso> listaAccesos;
+
+    public Expediente(){
+        listaAccesos = new ArrayList<Acceso>();
+        diagnostico = new Diagnostico(this, new Paciente());
+    }
 
     public Expediente(Paciente paciente){
+        assert paciente!=null;
+        listaAccesos = new ArrayList<Acceso>();
+        diagnostico = new Diagnostico(this, paciente);
+    }
 
+    void addAcceso(Acceso acceso){
+        assert acceso!=null;
+        listaAccesos.add(acceso);
+    }
+
+    void rmAcceso(Acceso acceso){
+        assert acceso!=null;
+        listaAccesos.remove(acceso);
+    }
+
+    public Enumeration<Acceso> getAcceso(){
+        return java.util.Collections.enumeration(listaAccesos);
+    }
+    
+    public Diagnostico getDiagnostico() {
+        return diagnostico;
+    }
+   
+    public void setDiagnostico(Diagnostico diagnostico) {
+        assert diagnostico!=null;
+        this.diagnostico = diagnostico;
     }
 }
